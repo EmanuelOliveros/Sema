455a446943e5d31968a0a17bb4db9c6b4fe4effc
emanuel oliveros <emanueloliverosmorales@gmail.com>
2018-09-18 20:38:32 -0500

    cambios

--- database.sql
+++ database.sql
@@ -27,4 +27,20 @@ UPDATE `sema`.`slider` SET `ruta`='F:\\WebApplication1\\web\\imagenes\\slider\\i
 UPDATE `sema`.`slider` SET `nombre`='Ropa', `ruta`='https://e.an.amtv.pe/util-e-interesante-se-lavar-ropa-nueva-antes-usarla-n268369-624x352-350731.jpg' WHERE `idslider`='1';
 UPDATE `sema`.`slider` SET `nombre`='Electrodomesticos ', `ruta`='https://static.vix.com/es/sites/default/files/styles/large/public/imj/hogartotal/C/Consejos-para-cuidar-los-electrodmesticos.jpg?itok=GYRNCsxv' WHERE `idslider`='2';
 UPDATE `sema`.`slider` SET `nombre`='Hardware', `ruta`='https://6469da.medialib.edu.glogster.com/8qDQm7rn1SOGnrnp0aE0/media/9e/9edc3a1525b14bf85606025da557d120f2b4a17c/perifericos.jpg' WHERE `idslider`='3';
-UPDATE `sema`.`slider` SET `nombre`='Software', `ruta`='https://lomasbuscadoenlaweb.com/wp-content/uploads/2017/03/Una-lista-de-diferentes-tipos-de-Software-de-computadora-con-ejemplos.jpg';
+UPDATE `sema`.`slider` SET `nombre`='Software', `ruta`='https://lomasbuscadoenlaweb.com/wp-content/uploads/2017/03/Una-lista-de-diferentes-tipos-de-Software-de-computadora-con-ejemplos.jpg' WHERE `idslider`='4';
+
+CREATE TABLE `sema`.`imagenes` (
+  `idimagenes` INT NOT NULL AUTO_INCREMENT,
+  `nombre` VARCHAR(45) NULL,
+  `ruta` VARCHAR(200) NULL,
+  PRIMARY KEY (`idimagenes`),
+  UNIQUE INDEX `imagenescol_UNIQUE` (`ruta` ASC));
+
+INSERT INTO `sema`.`imagenes` (`nombre`, `ruta`) VALUES ('?', '?');
+
+CREATE TABLE `sema`.`producto` (
+  `idproducto` INT NOT NULL AUTO_INCREMENT,
+  `nombre` VARCHAR(45) NULL,
+  `ruta` VARCHAR(200) NULL,
+  PRIMARY KEY (`idproducto`),
+  UNIQUE INDEX `imagenescol_UNIQUE` (`ruta` ASC));
